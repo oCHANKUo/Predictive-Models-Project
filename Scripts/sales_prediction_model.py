2fd79e47992e4a2105db1790028c2f5e3f81e83b
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-08-25 11:56:22 +0530

    database connection

--- Scripts/sales_prediction_model.py
+++ Scripts/sales_prediction_model.py
@@ -1,8 +1,38 @@
 # Prediction Target = line total
-# Features = OrderQty, UnitPrice, LineTotal
+# Features = OrderQty, UnitPrice, UnitPriceDiscount, ProductKey, CustomerKey, TerritoryKey, SalesPersonKey, OrderDateKey
 
 import pyodbc 
 from sklearn import linear_model
+import pandas as pd
+
+# First import data
+def get_connection():
+    conn = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "SERVER=localhost/MSSQLSERVER03;"
+        "DATABASE=DataWarehouseClassic;"
+        "UID=admin;"
+        "PWD=admin"
+    )
+    return conn
+
+def get_data():
+    conn = get_connection()
+    query = """
+    SELECT 
+        f.OrderQty, 
+        f.UnitPrice, 
+        f.UnitPriceDiscount, 
+        f.ProductKey, 
+        f.CustomerKey, 
+        f.TerritoryKey, 
+        f.SalesPersonKey, 
+        f.OrderDateKey 
+    FROM FactSales f
+    """
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
 
 
 
