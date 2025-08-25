cd0e42661650e7c6363f1fa9ba497f95266d0a43
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-08-25 12:20:26 +0530

    Importing and testing

--- Scripts/sales_prediction_model.py
+++ Scripts/sales_prediction_model.py
@@ -5,16 +5,19 @@ import pyodbc
 from sklearn import linear_model
 import pandas as pd
 
+model = linear_model.LinearRegression()
+
 # First import data
 # Function to connect to the database
 def get_connection():
     conn = pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=localhost/MSSQLSERVER03;"
+        "SERVER=localhost\\MSSQLSERVER03;"
         "DATABASE=DataWarehouseClassic;"
         "UID=admin;"
         "PWD=admin"
     )
+
     return conn
 
 # Function to import the data, it will connect using the previous function
@@ -22,19 +25,23 @@ def get_data():
     conn = get_connection()
     query = """
     SELECT 
-        f.OrderQty, 
-        f.UnitPrice, 
-        f.UnitPriceDiscount, 
-        f.ProductKey, 
-        f.CustomerKey, 
-        f.TerritoryKey, 
-        f.SalesPersonKey, 
-        f.OrderDateKey 
-    FROM FactSales f
+        LineTotal,
+        OrderQty, 
+        UnitPrice, 
+        UnitPriceDiscount, 
+        ProductKey, 
+        CustomerKey, 
+        TerritoryKey, 
+        SalesPersonKey, 
+        OrderDateKey 
+    FROM FactSalesOrderDetail
     """
     df = pd.read_sql(query, conn)
     conn.close()
     return df
+
+# import data
+df = get_data()
 
 target = 'LineTotal'
 y = df[target]
@@ -53,10 +60,13 @@ categorical_features = ['ProductKey',
 X_categorical = pd.get_dummies(df[categorical_features], drop_first=True)
 
 df['OrderDateKey'] = pd.to_datetime(df['OrderDateKey'])
-
 # Extract year, month, day, day of week, quarter
 df['OrderYear'] = df['OrderDateKey'].dt.year
 df['OrderMonth'] = df['OrderDateKey'].dt.month
 df['OrderDay'] = df['OrderDateKey'].dt.day
+time_features = ['OrderYear', 'OrderMonth', 'OrderDay']
+X_time = df[time_features]
 
-model = linear_model.LinearRegression()
+X = pd.concat([X_numeric, X_categorical, X_time], axis=1)
+
+print(X_time.head())
